95d33f5cce4429608b74c6f9503c46feccb46672
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-07 20:48:04 +0200

    Removed useless files

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Font generator
+            </div>
+        );
+    }
+}
+
+export default App;
