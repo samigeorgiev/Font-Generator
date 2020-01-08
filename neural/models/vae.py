[Fix] change variance in VAE to log-scale for numerical stability

neural/models/vae.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from .convolutional import ResDecoder, ResEncoder
+
 __all__ = [
     'VAE',
 ]
@@ -26,13 +28,13 @@ class VAE(nn.Module):
     beta : int, KLD weighting coefficent (see β-VAE)
     
     '''
-    
-    def __init__(self, latent_dim=20, beta=5):
+
+    def __init__(self, latent_dim=64, beta=5):
         super(VAE, self).__init__()
 
         self.latent_dim = latent_dim
         self.beta = beta
-        
+
         self.fc1 = nn.Linear(784, 400)
         self.fc21 = nn.Linear(400, latent_dim)
         self.fc22 = nn.Linear(400, latent_dim)
@@ -43,27 +45,29 @@ class VAE(nn.Module):
         h1 = F.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
 
-    def reparameterize(self, mu, sigma):
-        return mu + sigma * torch.randn_like(sigma)
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(logvar * 0.5)
+        eps = torch.randn_like(std)
+        return mu + std * eps
 
     def decode(self, z):
         h3 = F.relu(self.fc3(z))
         return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, sigma = self.encode(x)
-        z = self.reparameterize(mu, sigma)
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
         reconstruction = self.decode(z)
 
-        return mu, sigma, z, reconstruction
+        return mu, logvar, z, reconstruction
 
-    def loss_function(self, x, recon_x, mu, sigma):
-        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    def loss_function(self, x, recon_x, mu, logvar):
+        BCE = F.binary_cross_entropy(recon_x, x, reduction='mean')
 
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # KLD = -0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        KLD = -0.5 * torch.sum(1 + torch.log(sigma**2) - mu**2 - sigma**2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu**2 - logvar.exp())
 
         return BCE + self.beta * KLD