3cce6926ef291397d383a9085e2420c81568ad0c
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-11 10:01:07 +0200

    [Update] Added logo

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
+import Logo from 'components/Logo';
 import Toolbar from 'components/Navigation/Toolbar';
 
 import styles from './index.module.css';
 
-const layout = props => {
-    return (
-        <div className={styles.Layout}>
-            <header>
-                <Toolbar />
-            </header>
-            <main>
-                {props.children}
-            </main>
-            <footer>
-                <p>&copy; 2019 - {new Date().getFullYear()} All rights reserved | Sami</p>
-            </footer>
-        </div>
-    );
-};
+const layout = props => (
+    <div className={styles.Layout}>
+        <header>
+            <Logo />
+            <Toolbar/>
+        </header>
+        <main>
+            {props.children}
+        </main>
+        <footer>
+            <p>&copy; 2019 - {new Date().getFullYear()} All rights reserved | Sami</p>
+        </footer>
+    </div>
+);
 
 export default layout;
---
+++ client/src/components/Logo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FGLogo from 'assets/images/FGLogo.png';
+
+import styles from './index.module.css';
+
+const logo = props => (
+    <div className={styles.Logo}>
+        <img src={FGLogo} alt="FGLogo"/>
+    </div>
+);
+
+export default logo;
