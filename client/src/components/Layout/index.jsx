884a932ec9c87d6a8b309109fe7dc98778062f0d
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-11 09:23:57 +0200

    [Update] grid structure for layout component

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React from 'react';
+
+import Toolbar from 'components/Navigation/Toolbar';
 
 import styles from './index.module.css';
 
 const layout = props => {
     return (
-        <>
+        <div className={styles.Layout}>
             <header>
-
+                <Toolbar />
             </header>
-            {props.children}
+            <main>
+                {props.children}
+            </main>
             <footer>
-                Copyright
+                <p>&copy; 2019 - {new Date().getFullYear()} All rights reserved | Sami</p>
             </footer>
-        </>
+        </div>
     );
 };
 
