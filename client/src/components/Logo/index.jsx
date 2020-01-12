d4de8093a9f215d38aced6dd082e25f6db4017c3
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-12 10:14:36 +0200

    [Fix] fixed some code styling issues

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -6,7 +6,7 @@ import styles from './index.module.css';
 
 const logo = props => (
     <div className={styles.Logo}>
-        <img src={FGLogo} alt="FGLogo"/>
+        <img src={FGLogo} alt="FGLogo" />
     </div>
 );
 
