a30bed91c70e5ca40defdf85263f72779578d315
umnikos <alex.stefanov2@gmail.com>
2020-01-13 17:31:05 +0200

    [Update] add api status codes on request

--- api/main.py
+++ api/main.py
@@ -14,24 +14,25 @@ def register():
     print("REGISTER ATTEMPT!")
     print(username)
     if (db.username_exists(username)):
-        return {"success":False, "error":"username already taken"}
+        return {"success":False, "status":409, "error_message":"username already taken"}
     if (len(password) < 8):
-        return {"success":False, "error":"password too short"}
+        return {"success":False, "status":400, "error_message":"password too short"}
     if (len(password) > 64):
-        return {"success":False, "error":"password too long"}
+        return {"success":False, "status":400, "error_message":"password too long"}
     success = db.add_new_user(username, password)
     if success:
         print("REGISTERED NEW USER!")
-        return {"success": True}
-    return {"success":False, "error":"internal server error"}
+        return {"success": True, "status":200}
+    return {"success":False, "status":500, "error_message":"internal server error"}
 
 @app.route('/login', methods=['POST'])
 def login():
     # TODO
-    return {"success": True}
+    return {"success": True, "status":200}
 
 @app.route('/favourites/<uid>')
 def favourites(uid):
+    # TODO
     return db.get_favourites(uid)
 
 if __name__ == '__main__':
