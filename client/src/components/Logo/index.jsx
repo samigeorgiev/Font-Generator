85346d5b453cfa4aa63990156bf0807f02c0be9b
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-14 22:06:34 +0200

    [Update] Added google and facebook logos + added brightness control to logo component

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 
 import FGLogo from 'assets/images/FGLogo.png';
 import GitHubLogo from 'assets/images/GitHubLogo.png';
+import GoogleLogo from 'assets/images/GoogleLogo.png';
+import FacebookLogo from 'assets/images/FacebookLogo.png';
 
 import styles from './index.module.css';
 
 const logo = props => {
     const images = {
         'FGLogo': FGLogo,
-        'GitHubLogo': GitHubLogo
+        'GitHubLogo': GitHubLogo,
+        'Google': GoogleLogo,
+        'Facebook': FacebookLogo
     };
 
     return (
         <div className={styles.Logo}>
-            <img src={images[props.src]} alt="FGLogo"/>
+            <img src={images[props.src]} alt="FGLogo" style={{
+                filter: `brightness(${props.brightness})`
+            }} />
         </div>
     );
 };
