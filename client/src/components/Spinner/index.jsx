554feb0a0f848e6f7bcc357e032d430647c9fb70
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-15 16:12:40 +0200

    [Update] Added spinner

---
+++ client/src/components/Spinner/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+const spinner = props => (
+    <div className={styles.loader}>Loading...</div>
+);
+
+export default spinner;
