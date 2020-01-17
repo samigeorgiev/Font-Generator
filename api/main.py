43a286511590483da7f13efcd678aae1eb3f852d
umnikos <alex.stefanov2@gmail.com>
2020-01-17 17:54:30 +0200

    [Update] add api logging

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@ from secrets import jwt_secret
 from flask import Flask
 from flask import request, redirect, jsonify
 from flask_cors import CORS
+import logging
+from logging.handlers import RotatingFileHandler
 import database as db
 import datetime
 import jwt
@@ -12,31 +14,36 @@ CORS(app)
 
 @app.route('/api/register', methods=['POST'])
 def register():
-    print("REGISTER ATTEMPT!")
     data = json.loads(request.data)
     email = data['email']
     username = data['name']
     password = data['password']
-    print(username)
-    print(email)
-    if (db.email_exists(email)):
-        return jsonify({"success":False, "error_message":"email already registered"}), 409
-    if (db.username_exists(username)):
-        return jsonify({"success":False, "error_message":"username already taken"}), 409
-    if (len(password) < 8):
-        return jsonify({"success":False, "error_message":"password too short"}), 422
-    if (len(password) > 64):
-        return jsonify({"success":False, "error_message":"password too long"}), 422
-    success = db.add_new_user(email, username, password)
-    if success:
-        print("REGISTERED NEW USER!")
-        return jsonify({"success":True}), 201
-    return jsonify({"success":False, "error_message":"internal server error"}), 500
+    try:
+        app.logger.debug("Registration attempt ('%s', '%s')" % (username, email))
+        if (db.email_exists(email)):
+            app.logger.debug("Registration for '%s' failed - email already taken" % email)
+            return jsonify({"success":False, "error_message":"email already registered"}), 409
+        if (db.username_exists(username)):
+            app.logger.debug("Registration for '%s' failed - username already taken" % username)
+            return jsonify({"success":False, "error_message":"username already taken"}), 409
+        if (len(password) < 8):
+            app.logger.debug("Registration for '%s' failed - password too short" % username)
+            return jsonify({"success":False, "error_message":"password too short"}), 422
+        if (len(password) > 64):
+            app.logger.debug("Registration for '%s' failed - password too long" % username)
+            return jsonify({"success":False, "error_message":"password too long"}), 422
+            db.add_new_user(email, username, password)
+            app.logger.info("Registered new user ('%s', '%s')" % (username, email))
+            return jsonify({"success":True}), 201
+    except Exception as e:
+        app.logger.error("Error while processing registration ('%s', '%s')\n%s" % (username, email, e))
+        return jsonify({"success":False, "error_message":"internal server error"}), 500
 
 @app.route('/api/user_exists', methods=['GET'])
 def user_exists():
     data = json.loads(request.data)
     email = data['email']
+    app.logger.debug("Check if email '%s' exists" % email)
     return jsonify({"success":True, "response":db.email_exists(email)}), 200
 
 @app.route('/api/login', methods=['POST'])
@@ -44,8 +51,11 @@ def login():
     data = json.loads(request.data)
     email = data['email']
     password = data['password']
+    app.logger.debug("Attempt to login as '%s'" % email)
     if (not db.check_credentials(email, password)):
+        app.logger.debug("Invalid credentials on login as '%s'" % email)
         return jsonify({"success":False, "error_message":"email or password not correct"}), 401
+    app.logger.info("User '%s' logged in" % email)
     return encode_jwt(db.get_uid(email))
 
 def encode_jwt(uid):
@@ -74,4 +84,15 @@ def favourites(uid):
     return db.get_favourites(uid)
 
 if __name__ == '__main__':
+    today = datetime.date.today().strftime("%d%m%y")
+    handler = RotatingFileHandler(
+        "%s.log" % today,
+        maxBytes = 10000,
+        backupCount = 1
+    )
+    formatter = logging.Formatter(
+        "[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
     app.run()
