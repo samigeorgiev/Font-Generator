f809eb676e4ed8dc08eec0235e9dfd2a89a7e700
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-20 19:15:56 +0200

    [Update] added label

--- client/src/components/Slider/index.jsx
+++ client/src/components/Slider/index.jsx
@@ -4,15 +4,19 @@ import styles from './index.module.css';
 
 const slider = props => {
     return (
-        <input
-            type="range"
-            min={props.min}
-            max={props.max}
-            step={props.step}
-            className={styles.Slider}
-            value={props.value}
-            onChange={props.change}
-        />
+        <div className={styles.SliderContainer}>
+            <label htmlFor={props.name}>{props.name}</label>
+            <input
+                type="range"
+                id={props.name}
+                min={props.min}
+                max={props.max}
+                step={props.step}
+                className={styles.Slider}
+                value={props.value}
+                onChange={props.change}
+            />
+        </div>
     );
 };
 
