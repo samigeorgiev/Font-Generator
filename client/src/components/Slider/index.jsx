62765a6b13fac001619dbde29169a067860a5b12
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-20 17:36:16 +0200

    [Update] Added props to slider comp

--- client/src/components/Slider/index.jsx
+++ client/src/components/Slider/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 const slider = props => {
     return (
-        <input type="range" min="1" max="10" onChange={props.change} value={props.value} />
+        <input
+            type="range"
+            min={props.min}
+            max={props.max}
+            step={props.step}
+            className={styles.Slider}
+            value={props.value}
+            onChange={props.change}
+        />
     );
 };
 
