ad6790299c46fb3859b6d444c0d219692df2e051
Samuil Georgiev <sami.georgiev@gmail.com>
2020-01-21 21:41:48 +0200

    [Update] finished saved

--- client/src/pages/Saved/index.jsx
+++ client/src/pages/Saved/index.jsx
@@ -1,38 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import styles from './index.module.css';
 
 class Saved extends Component {
     state = {
+        loading: true,
+        error: null,
         fonts: null
+    };
+
+    deleteHandler = async id => {
+        const url = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_DELETE_FONT_PATH + '/' + id;
+        try {
+            await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': this.props.user.token
+                }
+            });
+            this.setState(prevState => ({
+                fonts: prevState.fonts.filter(font => font.id !== id)
+            }));
+        } catch (err) {
+            this.setState({
+                error: err
+            });
+        }
     };
 
     componentDidMount() {
         const url = process.env.REACT_APP_BASE_URL + process.env.REACT_APP_GET_SAVED_FONTS_PATH;
-        // fetch(url).then(data => data.json()).then(fonts => {
-        //     this.setState({
-        //         fonts: fonts
-        //     });
-        // }).catch(e => console.log(e));
+        fetch(url, {
+            headers: {
+                'Authorization': this.props.user.token
+            }
+        }).then(data => data.json()).then(fonts => {
             this.setState({
-                fonts: [
-                    { heading: 'dfdfgdfg', body: 'dsfgfs' },
-                    { heading: 'dfdfgdfg', body: 'dsfgfs' },
-                    { heading: 'dfdfgdfg', body: 'dsfgfs' },
-                    { heading: 'dfdfgdfg', body: 'dsfgfs' }
-                ]
+                fonts: fonts,
+                loading: false
             });
+        }).catch(err => this.setState({loading: false, error: err}));
     }
 
     render() {
+        if (this.state.error) {
+            throw this.state.error;
+        }
+
+        const pageContent = this.state.fonts && this.state.fonts.map(font => (
+                <div key={font.id} className={styles.FontContainer}>
+                    <p className={styles.HeadingFont}>Heading font: {font.heading}</p>
+                    <p className={styles.BodyFont}>Body font: {font.body}</p>
+                    <button className={styles.DeleteButton} onClick={() => this.deleteHandler(font.id)}>
+                        X
+                    </button>
+                </div>
+            ));
+
         return (
             <main className={styles.Saved}>
-                {this.state.fonts && this.state.fonts.map(font => (
-                    <div>
-                        <h2>Heading font: {font.heading}</h2>
-                        <h3>Body font: {font.body}</h3>
-                    </div>
-                ))}
+                {this.state.loading ? 'Loading..' : pageContent}
             </main>
         );
     }
