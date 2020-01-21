ed60f7a09d46877093564970cc23ddaa7801e799
Victor Velev <victorivelev@gmail.com>
2020-01-21 10:38:04 +0200

    [Update] add embeddings computer

---
+++ neural/compute_embeddings.py
@@ -0,0 +1,19 @@
+import torch
+
+from dataset import FontsLoader
+from utils import init_model
+
+
+if __name__ == '__main__':
+    device, net = init_model()
+    loaded_set = FontsLoader.get_set_loader()
+
+    print()
+    for i, batch in enumerate(loaded_set, 1):
+        embeddings = net.post_encoder( net.encoder(batch['image']) )
+
+        torch.save(embeddings, f'./font_embeddings/font-embeddings-batch-{i}.pt')
+        torch.save(batch['name'], f'./font_embeddings/font-names-batch-{i}.pt')
+
+        print(f'Saved font-embeddings-batch-{i}')
+    print()
--- neural/pair_font.py
+++ neural/pair_font.py
@@ -5,7 +5,6 @@ import torch
 from sklearn.neighbors import NearestNeighbors
 
 from utils import init_model
-from dataset import FontsLoader
 
 
 def contrastive_similarity(a, b):
@@ -28,14 +27,4 @@ def pair_font(base, contrast, n_neighbors=5,
 
 if __name__ == '__main__':
     device, net = init_model()
-    loaded_set = FontsLoader.get_set_loader()
-
-    print()
-    for i, batch in enumerate(loaded_set, 1):
-        embeddings = net.post_encoder( net.encoder(batch['image']) )
-
-        torch.save(embeddings, f'./font_embeddings/font-embeddings-batch-{i}.pt')
-        torch.save(batch['name'], f'./font_embeddings/font-names-batch-{i}.pt')
-
-        print(f'Saved font-embeddings-batch-{i}')
-    print()
+    
